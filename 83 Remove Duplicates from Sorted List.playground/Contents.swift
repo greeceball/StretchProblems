7dda0f949124455b7fff71dd579e9cf7e4e646c3
greeceball <griff33@gmail.com>
2020-06-17 09:38:19 -0600

    Morning Stretch Problem

---
+++ 83 Remove Duplicates from Sorted List.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+/*
+ Given a sorted linked list, delete all duplicates such that each element appear only once.
+ */
+
+
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        guard let head = head else { return nil }
+        guard let nextNode = head.next else {return head}
+        
+            if head.val != nextNode.val && nextNode != nil {
+                deleteDuplicates(nextNode)
+            }
+            if head.val == nextNode.val {
+                head.next = nextNode.next
+                deleteDuplicates(head)
+            }
+            
+        return head
+    }
+}
+
+
